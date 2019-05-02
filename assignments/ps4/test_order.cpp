c37fdec2770845a07f90ddba9a31b1fa223f06d1
Spencer Pease <spencer.p.816@gmail.com>
2019-05-02 02:12:02 -0700

    Update `test_order` to test nonzero matrix products

--- assignments/ps4/test_order.cpp
+++ assignments/ps4/test_order.cpp
@@ -25,40 +25,44 @@ TEST_CASE("Matrix operations", "[Zero]") {
   REQUIRE(Frobenius_norm(D) == 0.0);
 
 
-  SECTION("test randomize") {  
-    randomize(A); 
+  SECTION("test randomize") {
+    randomize(A);
     REQUIRE(Frobenius_norm(A) != 0.0);
-    
-    randomize(B); 
+
+    randomize(B);
     REQUIRE(Frobenius_norm(B) != 0.0);
-    
-    randomize(C); 
+
+    randomize(C);
     REQUIRE(Frobenius_norm(C) != 0.0);
-    
+
     randomize(D);
     REQUIRE(Frobenius_norm(D) != 0.0);
   }
-  SECTION("test zeroize") {  
+  SECTION("test zeroize") {
     zeroize(A);
     REQUIRE(Frobenius_norm(A) == 0.0);
-    
-    zeroize(B); 
+
+    zeroize(B);
     REQUIRE(Frobenius_norm(B) == 0.0);
-    
-    zeroize(C); 
+
+    zeroize(C);
     REQUIRE(Frobenius_norm(C) == 0.0);
-    
+
     zeroize(D);
     REQUIRE(Frobenius_norm(D) == 0.0);
-  }    
+  }
 }
 
 TEST_CASE("Matrix loop order", "[Order]") {
   size_t N = 256;
   Matrix A(N, N), B(N, N), C(N, N), D(N, N);
 
+  // Make sure computations are same
+  randomize(A);
+  randomize(B);
 
-  SECTION("test ijk (kind of)") {  
+
+  SECTION("test ijk (kind of)") {
     Matrix E = A * B;
     zeroize(C);
     matmat(A, B, C);
@@ -71,12 +75,12 @@ TEST_CASE("Matrix loop order", "[Order]") {
   SECTION("test ikj order") {
     zeroize(C);
     matmat_ikj(A, B, C);
-    REQUIRE(Frobenius_norm(D-C) < 1.e-12);    
+    REQUIRE(Frobenius_norm(D-C) < 1.e-12);
   }
 
   SECTION("test jki order") {
     zeroize(C);
     matmat_jki(A, B, C);
-    REQUIRE(Frobenius_norm(D-C) < 1.e-12);    
+    REQUIRE(Frobenius_norm(D-C) < 1.e-12);
   }
-}  
+}
