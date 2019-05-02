209e776ef3edaa19caf9ab147bd413296c8747e4
Spencer Pease <spencer.p.816@gmail.com>
2019-05-02 03:00:13 -0700

    Add row vs col access multiplication comparison

--- assignments/ps4/amath583_rvc.hpp
+++ assignments/ps4/amath583_rvc.hpp
@@ -1,6 +1,7 @@
 
 
 #include <random>
+#include <functional>
 #include "RowMatrix.hpp"
 #include "ColMatrix.hpp"
 
@@ -8,7 +9,7 @@ void   randomize(RowMatrix& A) {
   std::default_random_engine generator;
   std::uniform_real_distribution<double> distribution(-1.0, 1.0);
   static auto dice = std::bind(distribution, generator);
-  
+
   for (size_t i = 0; i < A.num_rows(); ++i) {
     for (size_t j = 0; j < A.num_cols(); ++j) {
       A(i, j) = dice();
@@ -21,7 +22,7 @@ void   randomize(ColMatrix& A) {
   std::default_random_engine generator;
   std::uniform_real_distribution<double> distribution(-1.0, 1.0);
   static auto dice = std::bind(distribution, generator);
-  
+
   for (size_t i = 0; i < A.num_rows(); ++i) {
     for (size_t j = 0; j < A.num_cols(); ++j) {
       A(i, j) = dice();
@@ -32,10 +33,22 @@ void   randomize(ColMatrix& A) {
 
 
 void   matmat(const RowMatrix& A, const RowMatrix& B, RowMatrix& C) {
-  /* WRITE ME */
+  for(size_t i = 0; i < C.num_rows(); i++) {
+    for(size_t k = 0; k < A.num_cols(); k++) {
+      for(size_t j = 0; j < C.num_cols(); j++) {
+  C(i, j) += A(i, k) * B(k, j);
+      }
+    }
+  }
 }
 
 
 void   matmat(const ColMatrix& A, const ColMatrix& B, ColMatrix& C) {
-  /* WRITE ME */
+  for(size_t j = 0; j < C.num_cols(); j++) {
+    for(size_t k = 0; k < A.num_cols(); k++) {
+      for(size_t i = 0; i < C.num_rows(); i++) {
+  C(i, j) += A(i, k) * B(k, j);
+      }
+    }
+  }
 }
